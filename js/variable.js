0821a4276703acc3a0f4cd674e3b347e1aea334c
22TalhaFaiz <138751622+22TalhaFaiz@users.noreply.github.com>
2023-08-08 02:14:01 +0500

    gt

--- js/variable.js
+++ js/variable.js
@@ -1,5 +1,5 @@
 let all_products = [
-  
+
   {
     "name": "Dawlance REF 91999 CHROME", "desc": "Vitamin Fresh Technology<br>5 Way cooling + 30% faster cooling<br>Optimized Fan with Deodorizing Filter<br>Side Illumination LED<br>Low voltage technology<br>A+ Energy Rating<br>12 years warranty<br>1 year extended warranty<br>low voltage compressor", "price": "152,000 PKR", "imgsrc": "img/dawlance/DAWLANCE_215-1.jpg", "imgsrc2": "img/dawlance/DAWLANCE_256-Black-1.jpg",
     "categories": ["electronics", 'fridge'], "imgsrc3": "img/dawlance/Vitaminfresh-R3-REF-LINEAR-11.jpg", "imgsrc4": "img/dawlance/Vitaminfresh-R3-REF-LINEART-12.jpg", "color": ["Black & Copper"], "id": 0
@@ -40,7 +40,9 @@ let all_products = [
   { "name": "Haier HWM 150-1789", "desc": "Brand Warranty Dimensions W*D*H : 600*575*950 Wash & Spin Capacity: 15 Kg Incredible Series Multi Programming Different Water Level sLED Tempered Panel Display Memory Backup Auto Restart Fuzzy Logic Black Control Panel ColorToughened Glass Top Lid Black Top Lid PCM Cabinet(Rust Free Bdoy).Normal Motor Pillow DrumDual Lint filters to capture more dirt.", "price": "105,000 PKR", "imgsrc": "img/haier 3/150-1789.jpg", "id": 21 },
   { "name": "Haier HWM 75-918​​​​12", "desc": "08 Multiple Programs LED Display Memory Backup RPM: 1300 Dimensions(W*D*H): 570*550*840 Dual Lint Filters Fuzzy Logic Black Control Panel Color Toughened Glass Top Lid Black Top Lid Silver Cabinet Color Steel Cabinet Normal Motor Pillow Drum", "price": "110,000 PKR", "imgsrc": "img/haier 4/hhh.png", "id": 22 },
   { "name": "Haier HWM 120-B14876", "desc": "Brand Warranty<br>Wash & Spin Capacity : 12 kg<br>ABT & UV Light Technology<br>Hygienic Wash<br>Luxury & Unique Style With One Touch Solution<br>Direct Drive (DD) Motor<br>Big Drum<br>Adjustable Thermostat<br>Airy Dry<br>Dimension(mm): 595x600x850<br>RPM: 1400<br>Smart Dual Spray<br>Stainless Steel Drum<br>Low Noise & No Vibration<br>Auto Restart<br>Compact Size<br>Water & Energy Efficient<br>LED Touch Screen", "price": "95,000 PKR", "imgsrc": "img/haier washing mach/wm.png", "id": 23 },
- { "name": "Haier H43D2M", "desc": "Screen Size (Inch) 43\" Resolution<br>Full HD<br>Category<br>H-Cast (Screen Mirroring)Haier H43D2M", "price": "90,000 PKR", "imgsrc": "img/tv/hai3/W020200914706851432574_450 (1).jpg", "imgsrc2": "img/tv/hai3/W020200914706851707239.jpg", "categories": ["electronics", 'tv', "televsion"], "imgsrc3": "img/tv/hai3/W020200914706851873561.jpg", "imgsrc4": "img/tv/hai3/W020200914706852012713.jpg", "color": ["black"], "id": 18 }
+  { "name": "Haier H43D2M", "desc": "Screen Size (Inch) 43\" Resolution<br>Full HD<br>Category<br>H-Cast (Screen Mirroring)Haier H43D2M", "price": "90,000 PKR", "imgsrc": "img/tv/hai3/W020200914706851432574_450 (1).jpg", "imgsrc2": "img/tv/hai3/W020200914706851707239.jpg", "categories": ["electronics", 'tv', "televsion"], "imgsrc3": "img/tv/hai3/W020200914706851873561.jpg", "imgsrc4": "img/tv/hai3/W020200914706852012713.jpg", "color": ["black"], "id": 18 },
+  
+
 ];
 
 
@@ -87,8 +89,8 @@ var haierp = [
 
 ];
 
-var samsungp =[
-  
+var samsungp = [
+
   { "name": "Samsung RS62R5001M9", "desc": "Gross Total(Liter):680L<br>Net Total(Liter):647L<br>Net Width(mm):912 mm<br>Net Case Height with Hinge(mm):1780 mm<br>Net Depth with Door Handle(mm):716 mm<br>Net Weight(kg):101 kg<br>Compressor:Digital Inverter Compressor<br>Energy Grade:2 Star", "price": "69,000 PKR", "imgsrc": "img/samsung/pk-ref-sbs-rs5000-rs62r5001m9-ut-frontopenwithfoodsilver-216617477.jpg", "imgsrc2": "img/samsung/pk-ref-sbs-rs5000-rs62r5001m9-ut-frontsilver-216617489.jpg", "categories": ["electronics", 'fridge'], "imgsrc3": "img/samsung/pk-ref-sbs-rs5000-rs62r5001m9-ut-lperspectivesilver-216617474.jpg", "imgsrc4": "img/samsung/pk-ref-sbs-rs5000-rs62r5001m9-ut-rperspectivesilver-216617475.jpg", "color": ["Black GD & INOX"], "id": 8 },
 
   { "name": "Samsung RS50N3913BC", "desc": "Gross Total(Liter):551L<br>Net Total(Liter):501L<br>Net Width(mm):912mm<br>Net Case Height with Hinge(mm):1789mm<br>Net Depth with Door Handle(mm):734mm<br>Net Weight(kg):106kg<br>Compressor:Digital Inverter Compressor<br>Energy Efficiency Class:A+", "price": "75,000 PKR", "imgsrc": "img/samsung digital inverter/s.jpg", "imgsrc2": "img/samsung digital inverter/s2.jpg", "categories": ["electronics", 'fridge'], "imgsrc3": "img/samsung digital inverter/s3.jpg", "imgsrc4": "img/samsung digital inverter/samsung.jpg", "color": ["Black GD & INOX"], "id": 9 },
@@ -101,7 +103,24 @@ var samsungp =[
 
 ];
 
-var dawlancep =[
+var dawlancep = [
+  {
+    "name": "Dawlance REF 91999 CHROME", "desc": "Vitamin Fresh Technology<br>5 Way cooling + 30% faster cooling<br>Optimized Fan with Deodorizing Filter<br>Side Illumination LED<br>Low voltage technology<br>A+ Energy Rating<br>12 years warranty<br>1 year extended warranty<br>low voltage compressor", "price": "152,000 PKR", "imgsrc": "img/dawlance/DAWLANCE_215-1.jpg", "imgsrc2": "img/dawlance/DAWLANCE_256-Black-1.jpg",
+    "categories": ["electronics", 'fridge'], "imgsrc3": "img/dawlance/Vitaminfresh-R3-REF-LINEAR-11.jpg", "imgsrc4": "img/dawlance/Vitaminfresh-R3-REF-LINEART-12.jpg", "color": ["Black & Copper"], "id": 0
+  },
+
+  { "name": "Dawlance REF 9173 WB CHROME", "desc": "Hybrid Cooling Technology<br>Low voltage technology<br>30% faster <br>A+ Energy Rating<br>Optimised Door Pockets<br>Widest and Deepest<br>12 years warranty<br>1 year extended warrant<br>Hybrid cooling compressor", "price": "149,000 PKR", "imgsrc": "img/dawlance inv/Chrome-Black.png", "imgsrc2": "img/dawlance inv/Chrome-Copper.png", "categories": ["electronics", 'fridge'], "color": ["Black & Copper Gold"], "id": 1 },
+
+  { "name": "Dawlance SBS 600 INV", "desc": "AI Inverter with Smart Sensore<br>Metal Back Wall<br>Smart 4 in 1 Display<br>Air Clean Filter<br>Blue Fresh Light<br>Compressor:digital", "price": "120,000 PKR", "imgsrc": "img/dawlance ref/03.jpg", "imgsrc2": "img/dawlance ref/04.jpg", "categories": ["electronics", 'fridge'], "imgsrc3": "img/dawlance ref/05.jpg", "imgsrc4": "img/dawlance ref/Black-1.jpg", "color": ["Black GD & INOX"], "id": 2 },
+
+  { "name": "Dawlance REF 9150 LF", "desc": "30% larger freezer portion<br>Upto 35% energy saving<br>European technology with deeper and wider body<br>Anti-fungal removable gasket<br>12 years compressor warranty<br>1 year extended warranty<br>digital inverter compressor", "price": "190,000 PKR", "imgsrc": "img/dawlance refri/LF-GD-SERIES-0.jpg", "imgsrc2": "img/dawlance refri/LF-GD-SERIES-01.jpg", "categories": ["electronics", 'fridge'], "imgsrc3": "img/dawlance refri/LF-GD-SERIES-02-600x381.jpg", "imgsrc4": "img/dawlance refri/LF-GD-SERIES-03-600x381.jpg", "color": ["Brown,Burgendy,Red Gradient and Beige"], "id": 3 },
+
+
+  { "name": "Dawlance WD6G1H2N1C", "desc": "CAPACITY:12/8kg<br>DIMENSION (W X H X D, MM):600 x 850 x 675<br>USP1:TrueSteam<br>USP2:TurboWash<br>Tempered Glass Door:Yes", "price": "120,000 PKR", "imgsrc": "img/lg wm/N02_Dm-02.jpg", "imgsrc2": "img/lg wm/N01_Dm-01.jpg", "categories": ["electronics", 'Washing Machine'], "imgsrc3": "img/lg wm/N03_Dm-03.jpg", "imgsrc4": "Z07_Dz-07.jpg", "color": ["Black GD & INOX"], "id": 13 },
+
+  { "name": "Dawlance WDV2102WRV", "desc": "Inverter Direct Drive Motor<br>CAPACITY:20/12KG<br>DIMENSION (W X H X D, MM):700 x 990 x 770<br>USP1:6Motion DD<br>USP2:TurboWash", "price": "95,000 PKR", "imgsrc": "img/lg washingm/D-01-V2.jpg", "imgsrc2": "img/lg washingm/D-02-V2.jpg", "categories": ["electronics", 'Washing Machine'], "imgsrc3": "img/lg washingm/D-03-V2.jpg", "imgsrc4": "img/lg washingm/Z-04-V2.jpg", "color": ["White"], "id": 14 },
+  { "name": "Dawlance DWF 8200 X", "desc": "Features<br> Smart - IOT Bluetooth Connectivity<br> Child Lock Yes <br> Automatic Water Adjustment System Yes <br> Time Delay0-24h <br> Temperature Selection Yes", "price": "255,000 PKR", "imgsrc": "img/dawn wm/image (1).webp", "id": 24 },
   
 
+  
 ];
